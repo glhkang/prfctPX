fb9934101b01e508a68fe4652ddf650a96f32413
glhkang <glhkang@gmail.com>
2020-06-01 19:18:41 -0400

    finally got it up on heroku

--- frontend/prfctpx.jsx
+++ frontend/prfctpx.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Root from "./components/root";
-import configureStore from "./store/store";
+// import configureStore from "./store/store";
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
-  const store = configureStore();
-  ReactDOM.render(<Root store={store} />, root);
+  // const store = configureStore();
+  //look below.. change it
+  const store = {};
+  ReactDOM.render(<h1>hello</h1>, root);
 });
