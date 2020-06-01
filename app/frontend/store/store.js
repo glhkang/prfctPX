20aeba79daaeee108258e50c4039ad961ed3498b
glhkang <glhkang@gmail.com>
2020-06-01 18:20:09 -0400

    added some frontend and some backend

---
+++ app/frontend/store/store.js
@@ -0,0 +1,9 @@
+// import { createStore, applyMiddleware } from "redux";
+// import thunk from "redux-thunk";
+// // import RootReducer from
+
+// const configureStore = (preloadedState = {}) => {
+//   return createStore(RootReducer, preloadedState, applyMiddleware(thunk));
+// };
+
+// export default configureStore;
