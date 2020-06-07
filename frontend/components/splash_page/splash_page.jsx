fb6b4ebc8c465065b2535f2a1f326108db08bc4b
glhkang <glhkang@gmail.com>
2020-06-07 17:31:08 -0400

    made splash page prettier

--- frontend/components/splash_page/splash_page.jsx
+++ frontend/components/splash_page/splash_page.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { withRouter } from 'react-router';
+import { Link } from 'react-router-dom';
 
 class SplashPageContainer extends React.Component {
 
   render() {
     return (
       <div>
-        <div className="main-splash">
-          <div className="main-splash-content">
+        <div className='main-splash'>
+          <div className='main-splash-content'>
             <div>
               <h1>Discover and share the world's best photos</h1>
               <p>
@@ -16,7 +17,9 @@ class SplashPageContainer extends React.Component {
                 photography.
               </p>
             </div>
+            {/* <Link to="/signup">Sign up</Link> */}
           </div>
+          <div className='splash-cover'></div>
         </div>
       </div>
     );
@@ -24,18 +27,3 @@ class SplashPageContainer extends React.Component {
 }
 
 export default withRouter(SplashPageContainer);
-
-
-    // return (
-    //   <div className="main-all-content">
-    //     <img src={window.splashImage} className="main-splash-image" />
-    //     <div>
-    //       <h1>Discover and share the world's best photos</h1>
-    //       <p>
-    //         Get inspired with incredible photos from diverse styles and genres
-    //         around the world. We're not guided by fads—just great photography.
-    //       </p>
-    //     </div>
-    //     <div className="main-splash"></div>
-    //   </div>
-    // );
